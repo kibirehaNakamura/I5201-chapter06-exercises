7f7959e02667ed91feef06066cac87799a4e05b1
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-19 15:43:37 +0900

    Sample6_4_1.javaを追加

---
+++ Sample6_4_1/Sample6_4_1.java
@@ -0,0 +1,9 @@
+public class Sample6_4_1 {
+	public static void main(String[] args) {
+		int[] arrayScore = { 80, 70, 90 };
+		
+		for(int i= 0; i < arrayScore.length; i++) {
+			System.out.println(arrayScore[i]);
+		}
+	}
+}
