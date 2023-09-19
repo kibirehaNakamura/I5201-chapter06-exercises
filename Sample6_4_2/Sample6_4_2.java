0488d82d231c0446e5f4dfb7cbe2b8a97468b7a3
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-19 15:44:13 +0900

    Sample6_4_2.javaを追加(異常終了するプログラムです)

---
+++ Sample6_4_2/Sample6_4_2.java
@@ -0,0 +1,9 @@
+public class Sample6_4_2 {
+	public static void main(String[] args) {
+		int[] arrayScore = { 80, 70, 90 };
+		
+		for(int i = 0; i <= arrayScore.length; i++) {
+			System.out.println(arrayScore[i]);
+		}
+	}
+}
