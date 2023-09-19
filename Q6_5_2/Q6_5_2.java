14eb991ccd789c18390b0c76f3d34f1f5f674c17
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-19 16:03:55 +0900

    Q6_5_2.javaを追加

---
+++ Q6_5_2/Q6_5_2.java
@@ -0,0 +1,14 @@
+public class Q6_5_2 {
+	public static void main(String[] args) {
+		int[] arrayNo = new int[10];
+		
+		for(int i = 0; i < arrayNo.length; i++) {
+			arrayNo[i] = i + 1;
+		}
+		
+		for(int i = 0; i < arrayNo.length; i++) {
+			System.out.print(arrayNo[i] + " ");
+		}
+		System.out.println();
+	}
+}
