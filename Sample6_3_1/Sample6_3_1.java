65672e28f2063b5810247bf07c884c1c4aeb49de
Nakamura <kisy23013@kibireha.ac.jp>
2023-09-19 15:37:17 +0900

    Sample6_3_1.javaを追加

---
+++ Sample6_3_1/Sample6_3_1.java
@@ -0,0 +1,9 @@
+public class Sample6_3_1 {
+	public static void main(String[] args) {
+		int[] arrayScore = { 80, 70, 90 };
+		int total;
+		
+		total = arrayScore[0] + arrayScore[1] + arrayScore[2];
+		System.out.println(total);
+	}
+}
